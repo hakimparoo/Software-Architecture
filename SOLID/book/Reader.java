584ec7762d0bb91388c6a962f359d3032e29527e
Abdulhakim Mamu <69505770+hakimparoo@users.noreply.github.com>
2022-10-13 22:07:06 +0700

    Delete SOLID directory

--- SOLID/book/Reader.java
+++
@@ -1,5 +0,0 @@
-package com.solid.book;
-
-public interface Reader {
-    abstract void printToScreen();
-}
--- SOLID/lsp/Area.java
+++
@@ -1,4 +0,0 @@
-// package com.solid.lsp;
-public abstract class Area {
-    public abstract int getArea();
-}
